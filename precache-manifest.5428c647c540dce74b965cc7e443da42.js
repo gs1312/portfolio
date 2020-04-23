8a9aec8e24d460ae22bc28dbdec7947ba64f4276
gs1312 <garvitsharma13@gmail.com>
2020-04-23 20:42:45 +0530

    Updates

---
+++ precache-manifest.5428c647c540dce74b965cc7e443da42.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f768f815bd73a10715a2bc2e493359f9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "88743d42bb9416cc5ab2",
+    "url": "/portfolio/static/css/main.4724a70f.chunk.css"
+  },
+  {
+    "revision": "8b9b061efe6a2eea1d45",
+    "url": "/portfolio/static/js/2.79fac0d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.79fac0d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88743d42bb9416cc5ab2",
+    "url": "/portfolio/static/js/main.92d95b1d.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "8eb4ffd82b0f03d9fabc95eaf1e70cf5",
+    "url": "/portfolio/static/media/IMG.8eb4ffd8.jpg"
+  },
+  {
+    "revision": "065cb2c1afca3b5d269bfdc11cee435c",
+    "url": "/portfolio/static/media/inslogo.065cb2c1.jpeg"
+  },
+  {
+    "revision": "4923034e804c261d13406d8fb5036567",
+    "url": "/portfolio/static/media/linklogo.4923034e.png"
+  },
+  {
+    "revision": "26f57889c8e3f109bbdcab11e9b3c0bf",
+    "url": "/portfolio/static/media/ytlogo.26f57889.jpeg"
+  }
+]);
